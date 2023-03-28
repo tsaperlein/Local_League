f5870c857efd646aaf59de3bceae42c23f609faf
Michael Stefanioros <mstephanioros@gmail.com>
2023-03-28 11:58:31 +0300

    Corrections in standings

--- code/javascript/standings.js
+++ code/javascript/standings.js
@@ -1,9 +1,11 @@
-const btnEl = document.querySelector('.standings-choice-buttons-group-button');   // get the button element
+const btnEl = document.querySelectorAll('.standings-choice-buttons-group-button'); // get the button element
 
-btnEl.addEventListener('mouseover', (event) => { // when the mouse is over the button
-    const x = (event.pageX - btnEl.offsetLeft); // get the x position
-    const y = (event.pageY - btnEl.offsetTop);  // get the y position
+btnEl.forEach((btn) => {
+    btn.addEventListener('mouseover', (event) => { // when the mouse is over the button
+        const x = (event.pageX - btnEl.offsetLeft); // get the x position
+        const y = (event.pageY - btnEl.offsetTop);  // get the y position
 
-    btnEl.style.setProperty('--xPos', x + 'px'); // set the x position
-    btnEl.style.setProperty('--yPos', y + 'px'); // set the y position
+        btnEl.style.setProperty('--xPos', x + 'px'); // set the x position
+        btnEl.style.setProperty('--yPos', y + 'px'); // set the y position
+    });
 });
