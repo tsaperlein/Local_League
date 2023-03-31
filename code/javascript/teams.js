cbb9424a3fb3b2b241b33c6845fb3bc1bd2b2cda
Michael Stefanioros <mstephanioros@gmail.com>
2023-03-31 13:37:19 +0300

    teams

---
+++ code/javascript/teams.js
@@ -0,0 +1,4 @@
+const teamsData = [
+    {icon: "../../images/aek.png", lineup: "../../lineups/aek-lineup.png", fieldName: "Team field: OPAP Arena Αγιά Σοφιά", fieldLink: "../../fields/aek-opap-arena.jpg"},
+    {icon: "../../images/osfpBig.png", lineup: "../../lineups/aek-lineup.png", fieldName: "Team field: OPAP Arena Αγιά Σοφιά", fieldLink: "../../fields/aek-opap-arena.jpg"}
+];
