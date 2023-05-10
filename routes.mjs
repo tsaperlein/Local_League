bcbbd0ea97c279e228c0546ae929ada38f249bdc
Michael Stefanioros <mstephanioros@gmail.com>
2023-05-10 21:34:12 +0300

    work on server

--- routes.mjs
+++ routes.mjs
@@ -2,7 +2,11 @@ import express from 'express'
 import obj from './models/teamData.mjs'
 
 const { Team } = obj;
-
+let teamsData = {}
+Team.find().sort({ rank: 1 }).then(result =>{
+    teamsData = {teams: result};
+})
+.catch(err => console.log(err))
 
 const router = express.Router()
 
@@ -11,12 +15,19 @@ router.get('/main-page', (req, res) => res.render('main-page'))
 router.get('/schedule', (req, res) => res.render('schedule'))
 
 router.get('/standings', (req, res) => {
-    Team.find().sort({ rank: 1 }).then(result =>{
-        result.forEach(element => {
-            res.render('standings', { team: element })
-        });
-    });
-})
+    console.log(teamsData.teams);
+    res.render('standings', { team: teamsData.teams });
+});
+
+// const teamsData = {
+//     teams: [
+//     {name: 'AEK', rank: 1},
+//     {name: 'Aris', rank: 2},
+// ]}
+
+// router.get('/standings', (req, res) => {
+//     res.render('standings', { team: teamsData.teams })
+// })
 
 // router.get('/teams/team/:team', (req, res) => res.render('teams', { team: req.params.team }))
 
