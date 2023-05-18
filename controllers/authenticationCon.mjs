153d7a750d0472277cff88bb0a5dff183db77a4b
Michael Stefanioros <mstephanioros@gmail.com>
2023-05-18 18:31:54 +0300

    Authentication works perfectly

--- controllers/authenticationCon.mjs
+++ controllers/authenticationCon.mjs
@@ -5,26 +5,53 @@ const { User } = userObj;
 
 const createUser = (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, 10);
-    const user = new User(req.body);
-
-    user.save()
-        .then((result) => {
-            res.redirect('/Local-League/main-page');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+    User.findOne({ username: req.body.username }).lean().then((user) => {
+        if(user != null) {
+            res.status(500).send("Username already exists");
+        }
+        else if(user == null) {
+            User.findOne({ email: req.body.email }).lean().then((result) => {
+                if(result != null) {
+                    res.status(500).send("An account with this email already exists");
+                }
+                else{
+                    const newUser = new User(req.body);
+                    newUser.save()
+                        .then((result) => {
+                            res.redirect('/Local-League/main-page');
+                            req.session.username = req.body.username;
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        })
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 }
 
 const authenticateUser = (req, res) => {
     if (req.body.usernameSignIn != undefined) {
         User.findOne({ username: req.body.usernameSignIn }, { username: 1, password:1 }).lean().then((user) => {
-            if(user.username == req.body.usernameSignIn && bcrypt.compareSync(req.body.passwordSignIn, user.password)) {
-                req.session.username = req.body.usernameSignIn;
-                res.redirect('/Local-League/main-page');
+            if(user != null) {
+                if(user.username == req.body.usernameSignIn && bcrypt.compareSync(req.body.passwordSignIn, user.password)) {
+                    req.session.username = req.body.usernameSignIn;
+                    res.redirect('/Local-League/main-page');
+                }
+                else {
+                    res.status(500).send("Wrong username or password");
+                    //res.redirect('/Local-League/main-page');
+                }
             }
             else {
-                res.redirect('/Local-League/main-page');
+                res.status(500).send("User not found");
+                //res.redirect('/Local-League/main-page');
             }
         })
         .catch((err) => {
@@ -32,7 +59,6 @@ const authenticateUser = (req, res) => {
         })
     } else if (req.body.username != undefined) {
         createUser(req, res);
-        req.session.username = req.body.username;
     }
     else {
         res.redirect('/Local-League/main-page');
